6aa1b37de1df40caa271ed270987e60f258ecdb8
Aravind <40268272+GeekRoot999@users.noreply.github.com>
2020-06-16 17:18:26 +0530

    robohash not updating

--- index.js
+++ index.js
@@ -27,8 +27,7 @@ function handleView(value){
     email.value = userDetail.email;
     address.value = userDetail.location.street.number+ " "+userDetail.location.street.name+ " "+userDetail.location.city+" "+userDetail.location.state+" "+userDetail.location.country+" "+userDetail.location.postcode+ " ";
     contact.value = userDetail.phone;
-    image.src = `https://robohash.org/test${userDetail.nat}?size=100x100`
-    console.log(userDetail.nat);
+    image.src = `https://robohash.org/test${userDetail.id.value}?size=100x100`
 }
 
 
