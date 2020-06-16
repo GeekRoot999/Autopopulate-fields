8721a55c204dee15cf4b36c8dbff4b4c5353ff2d
Aravind <40268272+GeekRoot999@users.noreply.github.com>
2020-06-16 17:21:57 +0530

    Robohash image problem fix

--- index.js
+++ index.js
@@ -27,7 +27,9 @@ function handleView(value){
     email.value = userDetail.email;
     address.value = userDetail.location.street.number+ " "+userDetail.location.street.name+ " "+userDetail.location.city+" "+userDetail.location.state+" "+userDetail.location.country+" "+userDetail.location.postcode+ " ";
     contact.value = userDetail.phone;
-    image.src = `https://robohash.org/test${userDetail.id.value}?size=100x100`
+    image.id = userDetail.id.value;
+    console.log(image.id);
+    image.src = `https://robohash.org/test${image.id}?size=100x100`
 }
 
 
